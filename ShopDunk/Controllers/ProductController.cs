b283f630acdeb32bf6c716f748af47efadf954df
thienngo2910 <ngodinhphucthien@gmail.com>
2025-11-08 13:17:52 +0700

    thêm sp

--- ShopDunk/Controllers/ProductController.cs
+++ ShopDunk/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using ShopDunk.Models;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -6,11 +7,13 @@ public class ProductController : Controller
 {
     private AppDbContext db = new AppDbContext();
 
-    // Hiển thị tất cả sản phẩm
+    // Hiển thị sản phẩm chia theo danh mục
     public ActionResult Index()
     {
-        var products = db.Products.ToList();
-        return View(products);
+        ViewBag.iPhones = db.Products.Where(p => p.Category == "iPhone").Take(4).ToList();
+        ViewBag.iPads = db.Products.Where(p => p.Category == "iPad").Take(4).ToList();
+        ViewBag.Macs = db.Products.Where(p => p.Category == "Mac").Take(4).ToList();
+        return View();
     }
 
     // Hiển thị chi tiết sản phẩm
@@ -30,5 +33,63 @@ public class ProductController : Controller
 
         ViewBag.CategoryName = name;
         return View(products);
+    }
+
+    // Hiển thị form thêm sản phẩm
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(product);
+    }
+
+    // Hiển thị form sửa sản phẩm
+    public ActionResult Edit(int id)
+    {
+        var product = db.Products.Find(id);
+        if (product == null) return HttpNotFound();
+        return View(product);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(product);
+    }
+
+    // Hiển thị xác nhận xóa
+    public ActionResult Delete(int id)
+    {
+        var product = db.Products.Find(id);
+        if (product == null) return HttpNotFound();
+        return View(product);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var product = db.Products.Find(id);
+        db.Products.Remove(product);
+        db.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
